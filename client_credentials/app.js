5f717ccbe641ae7bc10f8880fb23d45a35286647
efox12 <erikfox@Eriks-MacBook-Pro.local>
2018-04-08 14:29:13 -0700

    removed stuff

--- client_credentials/app.js
+++ client_credentials/app.js
@@ -11,16 +11,6 @@ var request = require('request'); // "Request" library
 //var SecretData = require('./SecretData.js');
 //let secretData = new SecretData();
 
-function getClientId(){
-  return '2153204389124b6097a0e86ca3a17f46'; // Your client id
-}
-function getClientSecret(){
-  return '55a4e744962a4d8e97bf54cc4bc44c32'; // Your secret
-}
-function getRedirectURI(){
-  return 'http://localhost:8888/callback'; // Redirect URI
-}
-
 var client_id = getClientId(); // Your client id
 var client_secret = getClientSecret(); // Your secret
 
