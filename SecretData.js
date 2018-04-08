16863fc5fae41c0d92653a74417b4fab1b14dd86
efox12 <erikfox@Eriks-MacBook-Pro.local>
2018-04-07 20:15:04 -0700

    added separate file for personal data

---
+++ SecretData.js
@@ -0,0 +1,8 @@
+
+module.exports =  class SecretData{
+    constructor() {
+        this.client_id = ''; // Your client id
+        this.client_secret = ''; // Your secret
+        this.redirect_Uri = ''; // Redirect URI
+    }
+};
