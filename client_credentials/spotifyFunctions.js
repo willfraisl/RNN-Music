5f3b52cdb1b8fcf7516883f8bc1e00bc4baffce3
efox12 <erikfox@Eriks-MacBook-Pro.local>
2018-04-09 17:05:39 -0700

    Added function for clustering a list of songs

--- client_credentials/spotifyFunctions.js
+++ client_credentials/spotifyFunctions.js
@@ -11,17 +11,6 @@ var spotifyApi = new SpotifyWebApi({
     redirectUri : getRedirectURI()
 });
 
-function getClientId(){
-    return '2153204389124b6097a0e86ca3a17f46'; // Your client id
-}
-function getClientSecret(){
-    return '55a4e744962a4d8e97bf54cc4bc44c32'; // Your secret
-}
-function getRedirectURI(){
-    return 'http://localhost:8888/callback'; // Redirect URI
-}
-
-
 // gets recomended songs based on given features
 function getRecomendations(options){
     getAuthorizationToken(function(token) { 
